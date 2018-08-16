f9331b5885a9258191235fc430d4dc36d0a05ba1
Amir_Afsharrad <afsharrad.a@gmail.com>
2018-08-16 19:40:04 +0430

    Some changes to Bode Plots PlottFFT2 function for to check two signals at the same time. It would be nice if you change the code in a way that Plotfft2 is not needed:))

--- ECG-Signal/preprocess.py
+++ ECG-Signal/preprocess.py
@@ -14,18 +14,32 @@ def plotfft(signal, fs):
     plt.plot(xf, 2. / len(signal) * np.abs(yf[0:len(signal) // 2]))
     plt.show()
 
-def mfreqz(b,a=1):
+def plotfft2(signal1, signal2, fs1, fs2, label1='', label2=''):
+
+    T1 = 1 / fs1  # Sampling Time
+    yf1 = fft(signal1)  # Fourier Transform
+    xf1 = np.linspace(0.0, 1.0 / (2 * T1), len(signal1) // 2)  # Frequency Axis
+    T2 = 1 / fs2 # Sampling Time
+    yf2 = fft(signal2)  # Fourier Transform
+    xf2 = np.linspace(0.0, 1.0 / (2 * T2), len(signal2) // 2)  # Frequency Axis
+    plt.figure()
+    plt.plot(xf1, 2. / len(signal1) * np.abs(yf1[0:len(signal1) // 2]))
+    plt.plot(xf2, 2. / len(signal2) * np.abs(yf2[0:len(signal2) // 2]))
+    plt.legend([label1, label2])
+    plt.show()
+
+def mfreqz(b,a=1,fs=1):
     w,h = sig.freqz(b,a)
     h_dB = 20 * np.log10 (abs(h))
     plt.subplot(211)
-    plt.plot(w/max(w),h_dB)
+    plt.plot(fs*w/(2*max(w)),h_dB)
     plt.ylim(-150, 5)
     plt.ylabel('Magnitude (db)')
     plt.xlabel(r'Normalized Frequency (x$\pi$rad/sample)')
     plt.title(r'Frequency response')
     plt.subplot(212)
     h_Phase = np.unwrap(np.arctan2(np.imag(h),np.real(h)))
-    plt.plot(w/max(w),h_Phase)
+    plt.plot(fs*w/(2*max(w)),h_Phase)
     plt.ylabel('Phase (radians)')
     plt.xlabel(r'Normalized Frequency (x$\pi$rad/sample)')
     plt.title(r'Phase response')
@@ -65,12 +79,12 @@ plotfft(ECG, fs)
 # FILTER ---------------------------------------------
 
 h = sig.firwin(101, [5, 40], width=None, window='hamming', pass_zero=False, scale=True, fs=fs)
-mfreqz(h)   # bode plot
+mfreqz(h,1,fs)   # bode plot
 
 ECG_filtered = sig.fftconvolve(h, ECG)
 
 
-plotfft(ECG_filtered, fs)
+plotfft2(ECG, ECG_filtered, fs, fs, 'Original ECG', 'Filtered ECG')
 
 plt.figure()
 plt.subplot(211)
